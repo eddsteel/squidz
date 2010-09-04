8cc26d87ab7299ac21f4b562ce0d39b17b0f65c9
Edd Steel <edward.steel@gmail.com>
2010-09-04 19:14:08 +0100

    Clean up javascript
      * use camelCase
      * clear up TODO

--- public/behaviour.js
+++ public/behaviour.js
@@ -1,16 +1,18 @@
 (function(){
-  function kv_s(map, key)
+  function kvS(map, key)
   {
     return key + "=" + map[key];
   }
 
   function render(obj, amount, callback)
   {
-    var query_string = "/render?" + [kv_s(obj, 'base'), kv_s(obj, 'target'), kv_s(obj, 'value'), 'amount='+amount].join('&');
-    $.get(query_string, callback);
+    var queryString = "/render?" + [kvS(obj, 'base'), 
+      kvS(obj, 'target'), kvS(obj, 'value'), 
+      'amount='+amount].join('&');
+    $.get(queryString, callback);
   }
 
-  function display_loading()
+  function displayLoading()
   {
     var message = '<article id="loadmessage">thinking hard...</article>';
     $('#message').prepend(message);
@@ -19,32 +21,32 @@
     $('#loadmessage').slideDown('slow');
   }
 
-  var display_error = function(result) {
-    display_message('error', result)
+  var displayError = function(result) {
+    displayMessage('error', result);
   }
 
-  function display_result(result, amount)
+  function displayResult(result, amount)
   {
 /* TODO Check all errors are served 4* or 5* status codes*/
    if (result['status'] === 'error')
    {
-     display_error(result.result.message)
+     displayError(result.result.message)
    }
    else if (result['status'] === 'ok')
    {
      var callback = function(html) {
-       display_message('result', html);
-       document.title = $('#message article .target-value')[0].innerHTML
+       displayMessage('result', html);
+       document.title = $('#message article:first .target-value').text()
      };
      render(result.result, amount, callback);
    }
    else
    {
-     display_message('error', 'something bad happened.');
+     displayMessage('error', 'something bad happened.');
    }
   }
 
-  function display_message(cssClass, message)
+  function displayMessage(cssClass, message)
   {
     $('#message')[0].className = "box " + cssClass;
     $('#loadmessage').replaceWith(message);
@@ -62,29 +64,34 @@
         dataType: "json",
         success: callback,
         error: function (xhr, ajaxOptions, thrownError){
-          display_error(xhr.responseText)
+          displayError(xhr.responseText)
         }
     });
   }
 
-  var fire = function()
-  {
-    display_loading();
-    var amount = $('#amount')[0].value;
-    var target = $('#target')[0].value;
-    var source = $('#source')[0].value;
-
+  function fireQuery(amount, target, source) {
+    displayLoading();
     var callback = function(json){
-      display_result(json, amount)
+      displayResult(json, amount)
     };
     query(source, target, amount, callback);
     return false;
+  }
+
+  var fire = function()
+  {
+    var amount = $('#amount').val();
+    var target = $('#target').val();
+    var source = $('#source').val();
+
+    return fireQuery(amount, target, source);
   };
 
-  $('#form').bind('submit', fire)
+  $('#form').bind('submit', fire);
+  createFlipLink($('#message article:first .flip a'))
+
 // TODO
 // * make the form submit to a # url instead, and use
 // that.
 // * remove [0]s and use proper jQuery stuff.
-// * animation
 })();
